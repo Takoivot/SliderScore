2c803b6d84ec8a565666d37aa3a615ce89d79651
Artur <alferovnet@mail.ru>
2022-07-13 12:20:49 +0300

    Initial Commit

---
+++ SliderScore/SliderScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderScoreApp.swift
+//  SliderScore
+//
+//  Created by Артур Сахбиев on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
